bbf45a794e20b622a17833a47f37c5d1d58ec434
Pedro Passos <pedro.passos081@gmail.com>
2025-10-08 23:37:17 -0300

    Initial commit do backend Strapi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,20 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      // Aqui está a mágica: autorizamos a URL do nosso front-end
+      origin: ["http://localhost:3000"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
