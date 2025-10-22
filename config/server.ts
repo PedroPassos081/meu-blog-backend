e9450d1d3a87e11296e62df95e86d904cd4253b9
PedroPassos081 <pedro.passos081@gmail.com>
2025-10-22 20:02:31 -0300

    Configure server host/port for Render (PORT=10000) and pin Node

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
-export default ({ env }) => ({
+// config/server.js
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  port: env.int('PORT', 10000), // Render precisa 10000
+  app: { keys: env.array('APP_KEYS') },
 });
