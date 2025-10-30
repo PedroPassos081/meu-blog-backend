85865d35f18d11ae701ea756d912f1f6bf4b1150
PedroPassos081 <pedro.passos081@gmail.com>
2025-10-30 20:40:23 -0300

    config

--- config/database.ts
+++ config/database.ts
@@ -1,12 +1,10 @@
-// config/database.ts
 export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: { require: true, rejectUnauthorized: false },
+      ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
     },
-    pool: { min: 0, max: 10 },
-    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    acquireConnectionTimeout: 60000,
   },
 });
