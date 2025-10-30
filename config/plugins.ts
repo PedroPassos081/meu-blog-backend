04a7352d6c712f61698ce1ba944c61c94e6c641d
PedroPassos081 <pedro.passos081@gmail.com>
2025-10-30 20:14:33 -0300

    Atualizando o middleware

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,14 +1,22 @@
 export default () => ({
     upload: {
         config: {
-            provider: 'cloudinary',
+            provider: '@strapi/provider-upload-cloudinary',
             providerOptions: {
-                cloud_name: process.env.CLOUDINARY_NAME,
-                api_key: process.env.CLOUDINARY_KEY,
-                api_secret: process.env.CLOUDINARY_SECRET,
+                cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+                api_key: process.env.CLOUDINARY_API_KEY,
+                api_secret: process.env.CLOUDINARY_API_SECRET,
+
             },
             actionOptions: {
-                upload: {},
+                upload: {
+                    // opções úteis
+                    folder: 'vetdev',           // organiza tudo numa pasta
+                    use_filename: true,         // mantém nome do arquivo
+                    unique_filename: false,     // não adiciona hash se não quiser
+                    overwrite: true,            // sobrescreve se mesmo nome
+                    resource_type: 'auto',      // aceita imagem/vídeo/pdf
+                },
                 delete: {},
             },
         },
